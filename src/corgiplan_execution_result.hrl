465841fc7330e22098c9faf6d34536cde12e6aca
Andrey Kateshov <andrey.kateshov@mendix.com>
2021-04-12 13:57:13 +0200

    [FIX] rearrage includes, remove init_db

--- src/corgiplan_execution_result.hrl
+++
@@ -1,4 +0,0 @@
--record(corgiplan_execution_result,
-        {job_server_name :: term(),
-         execution_time :: calendar:datetime1970(),
-         status :: success | max_retry_failure | marked_as_failure}).
